91d491666afd66d3af96070b6ec77a4767bf1c4a
mcg-cyber <m00nl04d3r@gmail.com>
2024-08-28 14:48:58 +0300

    new 04_mod

--- mod_04/01_divisible_by_three.py
+++ mod_04/01_divisible_by_three.py
@@ -3,8 +3,15 @@
 
 import random
 
+num = 0
+while num <= 1000:
+   if num % 3 == 0:
+     print(num)
+   num +=1
+"""
 while True:
     number_range = random.randint(1, 1000)
     if number_range % 3 == 0:
         print(number_range)
         # break
+"""
